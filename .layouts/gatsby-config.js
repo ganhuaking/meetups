72403e82be10841341d3172f2a2d80bfd66edc25
MilesChou <jangconan@gmail.com>
2023-01-02 23:56:29 +0800

    Test Obsidian

---
+++ .layouts/gatsby-config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const pathPrefix = "/";
+const siteMetadata = {
+  title: "蔣幹話軟體",
+  shortName: "Wiki",
+  description:
+    "台灣開發者的綜合型 Discord Server",
+  siteUrl: "https://meetups.ganhuaking.tw/",
+};
+module.exports = {
+  siteMetadata,
+  pathPrefix,
+  flags: {
+    DEV_SSR: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/..`,
+        ignore: [`**/\.*/**/*`],
+      },
+    },
+    {
+      resolve: "gatsby-theme-primer-wiki",
+      options: {
+        nav: [
+          {
+            title: "Github",
+            url: "https://github.com/ganhuaking/meetups",
+          },
+        ],
+        editUrl:
+          "https://github.com/ganhuaking/meetups/tree/main/",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.shortName,
+        start_url: pathPrefix,
+        background_color: `#f7f0eb`,
+        display: `standalone`,
+        icon: path.resolve(__dirname, "./static/logo.png"),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteMetadata.siteUrl,
+        sitemap: `${siteMetadata.siteUrl}/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [],
+      },
+    },
+  ],
+};
